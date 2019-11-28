ba192582eca4d1f11b190eaa50344e4046f86a89
Ivan Montero <ivanmontero@pie.local>
2019-11-28 13:04:17 -0800

    Add method to force lstm to learn entire sequence

--- audio_processing.py
+++ audio_processing.py
@@ -8,23 +8,33 @@ import numpy as np
 
 # Assumes data is n x seq, where n is the amount of audio files and seq varies
 # max_len is in terms of segment size
-def get_batch(data, max_len, batch_size, device, segment_size=1):
+def get_batch(data, max_len, batch_size, device, segment_size=1, full=False):
     batch = data[np.random.choice(data.shape[0], batch_size, replace=True)]
 
-    len1 = random.randint(1, max_len - 1)
-    len2 = random.randint(1, max_len - len1)
+    if not full:
+        len1 = random.randint(1, max_len - 1)
+        len2 = random.randint(1, max_len - len1)
 
-    len1 *= segment_size
-    len2 *= segment_size
+        len1 *= segment_size
+        len2 *= segment_size
 
-    p, n = np.zeros((batch_size, len1)), np.zeros((batch_size, len2))
+        p, n = np.zeros((batch_size, len1)), np.zeros((batch_size, len2))
 
-    for i in range(batch_size):
-        start = random.randint(0, batch[i].shape[0] - len1 - len2)
-        p[i] = batch[i][start:start+len1]
-        n[i] = batch[i][start+len1:start+len1+len2]
+        for i in range(batch_size):
+            start = random.randint(0, batch[i].shape[0] - len1 - len2)
+            p[i] = batch[i][start:start+len1]
+            n[i] = batch[i][start+len1:start+len1+len2]
 
-    return torch.tensor(p).to(device).float(), torch.tensor(n).to(device).float()
+        return torch.tensor(p).to(device).float(), torch.tensor(n).to(device).float()
+    else:
+        min_len = ((min([batch[i].shape[0] for i in range(batch_size)]) - max_len*segment_size) // segment_size) - 1
+        n_start = max_len*segment_size
+        n_end = min_len*segment_size + n_start
+        p, n = np.zeros((batch_size, n_start)), np.zeros((batch_size, min_len*segment_size))
+        for i in range(batch_size):
+            p[i] = batch[i][:n_start]
+            n[i] = batch[i][n_start:n_end]
+        return torch.tensor(p).to(device).float(), torch.tensor(n).to(device).float()
 
 
 # === below is gan ===
