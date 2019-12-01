dc625246644740c68e02dcda807dafc4e2a72808
Ivan Montero <ivanmontero@pie.local>
2019-11-30 23:37:09 -0800

    Parallelized midi loading

--- midinet.py
+++ midinet.py
@@ -5,34 +5,36 @@ from music21 import converter, instrument, note, chord
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import multiprocessing
+import os
 
-def get_notes():
+def get_notes_from_file(file):
     """ Get all the notes and chords from the midi files in the ./midi_songs directory """
     notes = []
 
-    for file in glob.glob("midis/*.mid"):
-        print("Parsing %s" % file)
+    midi = converter.parse(file)
 
-        midi = converter.parse(file)
+    notes_to_parse = None
 
-        notes_to_parse = None
+    try: # file has instrument parts
+        s2 = instrument.partitionByInstrument(midi)
+        notes_to_parse = s2.parts[0].recurse() 
+    except: # file has notes in a flat structure
+        notes_to_parse = midi.flat.notes
 
-        try: # file has instrument parts
-            s2 = instrument.partitionByInstrument(midi)
-            notes_to_parse = s2.parts[0].recurse() 
-        except: # file has notes in a flat structure
-            notes_to_parse = midi.flat.notes
-
-        for element in notes_to_parse:
-            if isinstance(element, note.Note):
-                notes.append(str(element.pitch))
-            elif isinstance(element, chord.Chord):
-                notes.append('.'.join(str(n) for n in element.normalOrder))
-
-        # TODO: Write this to file so it's faster
+    for element in notes_to_parse:
+        if isinstance(element, note.Note):
+            notes.append(str(element.pitch))
+        elif isinstance(element, chord.Chord):
+            notes.append('.'.join(str(n) for n in element.normalOrder))
     return notes
 
-notes = get_notes()
+
+def get_notes():
+    # TODO: Write this to file so it's faster
+    return multiprocessing.Pool(os.cpu_count()).map(get_notes_from_file, glob.glob("midis/*.mid"))
+
+notes = [n for s in get_notes() for n in s]
 print(notes)
 n_vocab = len(set(notes))
 pitchnames = sorted(set(item for item in notes))
