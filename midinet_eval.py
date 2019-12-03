321ee9b9dcb714fc8cc347c5fa3e97f6a5f39ad9
ivanmontero <ivanspmontero@gmail.com>
2019-12-02 21:30:14 -0800

    Tweaks to work with CUDA and multiprocessing

--- midinet_eval.py
+++ midinet_eval.py
@@ -4,29 +4,24 @@ from music21 import instrument, note, stream, chord
 from midinet import MidiNet
 from midinet import get_notes
 from midinet import generate_beats
+import os
 
 # TODO:
 # 1. load model from checkpoint
 # 2. eval final model code
 # 3. add create midi code and output to output folders
 
-FEATURE_SIZE = 512
-WEIGHTS_PATH = 'checkpoints/midinet.pt' #TODO: add in correct weight file for reconstruction
+FEATURE_SIZE = 2048
+WEIGHTS_PATH = 'checkpoints/032.pt' #TODO: add in correct weight file for reconstruction
 # SEQUENCE_LENGTH = 200
 TEMPERATURE = 3
 
-notes = [n for s in get_notes() for n in s]
-n_vocab = len(set(notes))
-pitchnames = sorted(set(item for item in notes))
-note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
-int_to_note = {val: key for key, val in note_to_int.items()}
-
-def load_model_from_checkpoint(checkpoint, device):
+def load_model_from_checkpoint(checkpoint, note_to_int, device):
     model = MidiNet(len(note_to_int), FEATURE_SIZE)
     model.load_state_dict(torch.load(checkpoint, map_location=device))
-    return model
+    return model.to(device)
 
-def eval_final_model(model, device):
+def eval_final_model(model, note_to_int, int_to_note, device):
     seed_words = 'A3 G#3 B4 F#6 E-5 B4 G#3 G#3 F#6 E-5 F#6 B4 B4 G#3 F#6 G#3 B6 E-5 E-5 G#3 F#6 E-5 B4 E-5 F#6 G#3 B4 B4 E-5 G#3 E-5 G3 G#3 B4 E-5 F#6 E-5 F#6 E-5 B4 G#3 G3 G#3 F#6 B4 E-5 E-5 B4 F#6 E-5 B4 F#6 E-5 B4 B6 E-5 E-5 B4 E-5 F#6 E-5 G#3 F#6 F#6 E-5 G#3 B4 E-5 B4 F#6 F#6 E-5'.split(
         " ")
     sequence_length = 256
@@ -70,14 +65,28 @@ def create_midi(prediction_output, sampling_strategy):
         print("Error! Invalid sampling strategy")
 
 def main():
+    if not os.path.exists('notes.txt'):
+        print("Reading midi files")
+        notes = [n for s in get_notes() for n in s]
+        with open("notes.txt", "w+") as f:
+            f.write(" ".join(notes))
+    else:
+        print("Restoring notes")
+        with open('notes.txt', 'r') as f:
+            notes = f.read().split(" ")
+    
+    n_vocab = len(set(notes))
+    pitchnames = sorted(set(item for item in notes))
+    note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
+    int_to_note = {val: key for key, val in note_to_int.items()}
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print('Using device: {}'.format(device))
     
     print('Loading model from checkpoint...')
-    model = load_model_from_checkpoint(WEIGHTS_PATH, device)
+    model = load_model_from_checkpoint(WEIGHTS_PATH, note_to_int, device)
     
     print('Evaluating model...')
-    sample_beats, beam_beats = eval_final_model(model, device)
+    sample_beats, beam_beats = eval_final_model(model, note_to_int, int_to_note, device)
     
     print('Saving results...')
     create_midi(sample_beats, 'sample')
