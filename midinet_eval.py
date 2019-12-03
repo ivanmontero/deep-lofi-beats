4ef2c96bb9cf7034eba63b1242c1241af58041fb
Anirudh Canumalla <anirudhcan@outlook.com>
2019-12-02 16:22:48 -0800

    Create midinet_eval.py

---
+++ midinet_eval.py
@@ -0,0 +1,9 @@
+import torch
+import numpy as np
+
+from midinet import MidiNet
+
+# TODO:
+# 1. load model from checkpoint
+# 2. eval final model code
+# 3. add create midi code and output to output folders
